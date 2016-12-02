6b26be8fa53955302402320399b8a39a023fbd2f
nitros12 <bsimms.simms@gmail.com>
2016-12-02 23:02:40 +0000

    added share spamming

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+import time
+import random
 
 import fb
 import requests
@@ -18,15 +20,17 @@ class Spammer:
     :share_conf: list of pages to grab posts from and share to your wall
     """
 
-    def __init__(self, own_id: str, api_key: str, comment_conf: dict, share_ids: list):
+    def __init__(self, own_id: str, api_key: str, comment_conf: dict, share_ids: dict):
         self._own_id = own_id
         self._api_key = api_key
         self._graph = fb.graph.api(api_key)
         self._comment_conf = comment_conf
-        self.share_ids = share_ids
+        self._share_ids = share_ids
         self._request = "https://graph.facebook.com/v2.3/{0._own_id}/home?fields=from&format=json&access_token={0._api_key}".format(
             self)
         self._current_request = self._request
+        for i in self._share_ids:
+            self._share_ids[i] = "https://graph.facebook.com/v2.3/{0}/posts?format=json&access_token={1._api_key}".format(i, self)
 
     def do_comments(self, limit=250):
         request = requests.get(
@@ -44,14 +48,32 @@ class Spammer:
                 self._graph.publish(cat="comments", id=i[
                                     "id"], message=self._comment_conf[user_id])
 
+    def do_shares(self):
+        for i in self._share_ids:
+            link = self._share_ids[i]
+            request = requests.get(link)
+            print(link)
+            if not request.status_code == 200:
+                raise Exception("Requests didn't get 200 response, instead got: {}".format(request.status_code))
+            data = request.json()
+            self._share_ids[i] = data["paging"]["next"]
+
+            for p in data["data"]:
+                post = p.get("link")
+                if post:
+                    self._graph.publish(cat="feed", id=self._own_id, message="wewlad", link=post, caption="memes",
+                                        description="ayy lmao")
+                    print("Sharing post: {}".format(post))
+                    time.sleep(random.randint(3, 9))
+            time.sleep(random.randint(4, 7))
 
 if __name__ == "__main__":
 
     with open("config.json") as jsonFile:
         config = json.load(jsonFile)
 
-
     mySpammer = Spammer(config["self_id"], config["api_key"], config[
                         "comment_conf"], config["share_ids"])
 
-    mySpammer.do_comments()
+    #mySpammer.do_comments()
+    mySpammer.do_shares()
